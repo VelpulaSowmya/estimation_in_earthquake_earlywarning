18f94c53bb21d827db302dc3c9c24e84caf91cc6
VelpulaSowmya <sowmyavelpula31@gmail.com>
2026-07-11 16:05:19 +0530

    Update settings.py

--- estimation_in_earthquake_earlywarning/settings.py
+++ estimation_in_earthquake_earlywarning/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'm+1edl5m-5@u9u!b8-=4-4mq&o1%agco2xpl8c!7sn7!eowjk#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
