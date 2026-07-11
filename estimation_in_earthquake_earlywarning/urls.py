0254bca95a06bd17805f4de2d771dcf8638a6c1c
VelpulaSowmya <sowmyavelpula31@gmail.com>
2026-07-11 15:49:57 +0530

    Update urls.py

--- estimation_in_earthquake_earlywarning/urls.py
+++ estimation_in_earthquake_earlywarning/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 from Remote_User import views as remoteuser
 from estimation_in_earthquake_earlywarning import settings
